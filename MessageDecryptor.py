bf75b9b24b242ad18f72a206564e4e6d9ca44608
Kenji1504 <gamboakenleam1015@gmail.com>
2023-04-18 13:24:53 +0800

    Pseudocode

---
+++ MessageDecryptor.py
@@ -0,0 +1,14 @@
+#Ken Leam G. Gamboa
+#BSCPE 1-5
+#This is a program that decrypts some specified characters from the user's input.
+
+#Print an introduction
+#ask for user's input
+#check each character from the user's input
+#if there's "*", change to "a"
+#if there's "&", change to "e"
+#if there's "#", change to "i"
+#if there's "+", change to "o"
+#if there's "!", change to "u"
+#Print the decrypted message
+#Print an ender for the program
